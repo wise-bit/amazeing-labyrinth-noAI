55d04d0674c902c701a9e2a21071a900b014bcce
ShrillP <1006shrillpatel@gmail.com>
2018-11-09 13:58:36 -0500

    WHYYYY

--- Labyrinth/src/deck.java
+++ Labyrinth/src/deck.java
@@ -1,5 +1,2 @@
 public class Deck {
-
-
-
 }
--- Labyrinth/src/main.java
+++ Labyrinth/src/main.java
@@ -1,11 +1,8 @@
 public class Main {
   
-
     public static void main(String[] args) {
 
         Player[] players = new Player[4];
-
-        new HomePageGUI();
 
     }
   
